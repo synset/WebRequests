6b58782e7f4e42610d946d0b4c0904628f3be929
synset <synnove.settman@yahoo.com>
2020-09-10 11:55:25 +0200

    Add project files.

---
+++ WebRequests/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebRequests.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
